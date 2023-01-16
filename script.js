83a19a653877eb79cd7212591815466ccebd4b95
austin-bagwell <austinbagwell.dev@gmail.com>
2023-01-15 21:28:29 -0500

    define etchasketch class, basic styles

---
+++ script.js
@@ -0,0 +1,23 @@
+"use strict";
+
+class EtchASketch {
+  constructor(size) {
+    this.size = size;
+  }
+
+  //   create a grid of divs
+  makeCanvas() {
+    const size = this.size;
+    let div = `<div class="pixel">X</div>`;
+    const row = div.repeat(size);
+    const rows = [];
+    for (let i = 0; i < size; i++) {
+      rows.push(`<div class="row">${row}</div>`);
+    }
+    return rows.join().replaceAll(",", "");
+  }
+}
+
+const sketchContainer = document.querySelector(".sketch-container");
+const sketch = new EtchASketch(4);
+sketchContainer.insertAdjacentHTML("afterbegin", sketch.makeCanvas());
