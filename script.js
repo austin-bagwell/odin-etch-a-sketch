b6fdc7eb371a4eadc1d7925879f3a0e32d25e3e0
austin-bagwell <austinbagwell.dev@gmail.com>
2023-01-22 17:30:13 -0500

    first stab at coloring pixels

--- script.js
+++ script.js
@@ -21,3 +21,15 @@ class EtchASketch {
 const sketchContainer = document.querySelector(".sketch-container");
 const sketch = new EtchASketch(16);
 sketchContainer.insertAdjacentHTML("afterbegin", sketch.makeCanvas());
+
+const rows = document.querySelectorAll(".row");
+const pixels = document.querySelectorAll(".pixel");
+
+// TODO use event bubbling to add a new background color to e.target using this as the callback
+function colorPixel(color) {}
+
+// should work like this to begin with, but use a named func in callback
+sketchContainer.addEventListener("click", function (e) {
+  console.log(`clicked on: `, e.target.classList);
+  e.target.style.backgroundColor = "black";
+});
